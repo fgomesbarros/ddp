001940e6d891831394d6342e054d4f9ffcb8e5bd
Fabio Barros <fgomesbarros@gmail.com>
2016-10-10 18:42:38 -0300

    version #2

--- 01-get_data.R
+++ 01-get_data.R
@@ -8,8 +8,9 @@
 rm(list = ls())
 
 # Sets url variables
-gdp_url <- "ftp://ftp.ibge.gov.br/Pib_Municipios/2010_2013/base/base_txt.zip"
+gdp_url <- "ftp://ftp.ibge.gov.br/Pib_Municipios/2010_2013/base/base_xls.zip"
 geo_url <- "ftp://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/localidades/Geomedia_MDB/BR_Localidades_2010_v1.mdb"
+
 
 # Creates data dir
 if(!file.exists("data")){
@@ -18,9 +19,9 @@ if(!file.exists("data")){
 
 # Download files
 ## GDP
-download.file(url = gdp_url, destfile = "data/base.txt.zip")
-unzip(zipfile = "data/base.txt.zip", exdir = "data")
-file.remove("data/base.txt.zip")
+download.file(url = gdp_url, destfile = "data/base.xls.zip")
+unzip(zipfile = "data/base.xls.zip", exdir = "data")
+file.remove("data/base.xls.zip")
 
 ## Geospatial data
 download.file(url = geo_url, destfile = "data/BR_Localidades_2010_v1.mdb")
