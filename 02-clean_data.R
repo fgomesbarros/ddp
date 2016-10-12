9cbe056660e2fdd429a3c1182544987cbd7dabe2
fgomesbarros <fgomesbarros@gmail.com>
2016-10-11 22:32:43 -0300

    Change xlxs package for gdata

--- 02-clean_data.R
+++ 02-clean_data.R
@@ -7,7 +7,7 @@
 # Sets up enviroment
 rm(list = ls())
 require(Hmisc)
-require(xlsx)
+require(gdata)
 require(dplyr)
 
 # Sets url variables
@@ -15,23 +15,27 @@ gdp_file <- "data/base.txt"
 geo_file <- "data/BR_Localidades_2010_v1.mdb"
 
 gdp_columns <- c("year", "state_id", "state_name", "county_id", "county_name",  
-                 "GDP", "GPD_per_capita")
+                 "GDP", "GDP_per_capita")
 
-# Cleans gdp_data
-gdp <- read.xlsx2(file = "data/base.xls", sheetIndex = 1, 
-                  stringsAsFactors = FALSE)
+# Reads GDP data
+gdp <- read.xls(xls = "data/base.xls", sheet = 1, stringsAsFactors = FALSE, 
+                encoding = "latin1", dec = ",")
 
+# Cleans GDP data
 ## Filters columns
 gdp <- gdp[, c(1:5, 17, 19)]
 names(gdp) <- gdp_columns
 
 ## Filters rows and colunms
 gdp <- gdp %>%
-  filter(state_id == "26" & year == "2013") %>%
-  select(one_of(c("county_id", "county_name", "GDP", "GPD_per_capita"))) %>%
-  mutate(county_id = as.numeric(county_id)) %>%
-  mutate(GDP = as.numeric(GDP)) %>%
-  mutate(GPD_per_capita = as.numeric(GPD_per_capita))
+    filter(state_id == "26" & year == "2013") %>%
+    select(county_id, county_name, GDP, GDP_per_capita) %>%
+    mutate(county_id = as.numeric(county_id)) %>% 
+    mutate(GDP = gsub(pattern = ",", replacement = "", x = GDP)) %>%
+    mutate(GDP = as.numeric(GDP)) %>%
+    mutate(GDP_per_capita = gsub(pattern = ",", replacement = "", 
+                                 x = GDP_per_capita)) %>%
+    mutate(GDP_per_capita = as.numeric(GDP_per_capita))
   
 # Cleans geospatial data
 geospatial <- mdb.get(file = geo_file, tables = "BR_Localidades_2010_v1")
